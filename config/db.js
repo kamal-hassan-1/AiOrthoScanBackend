a7b6ee89badc3cf77bae4f8dffb6db43761eea9e
kamal-hassan-1 <115949420+kamal-hassan-1@users.noreply.github.com>
2025-06-09 18:03:25 +0500

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -5,16 +5,12 @@ let client;
 
 module.exports = async () => {
     const connString = process.env.MONGO_URI;
-    if (!connString) throw new Error('MONGODB_CONNECTION_STRING environment variable is not set');
-
+    if (!connString) throw new Error('mongodb connection string in .env is not set');
     if (!client) {
         client = new MongoClient(connString);
-
         await client.connect();
         console.log('Connected to MongoDB');
-
         db = client.db();
     }
-
     return db;
 }
